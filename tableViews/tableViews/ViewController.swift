bfcbf47575d863741793067bd48b0d7c5ec65d61
Doug Wells <dougwells@gmail.com>
2016-11-21 07:42:54 -0700

    iOS. TableViews. Add basic tableview

--- tableViews/tableViews/ViewController.swift
+++ tableViews/tableViews/ViewController.swift
@@ -8,7 +8,28 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    var cellContent = ["Savannah","Sierra", "Linda","Doug"]
+    
+    internal func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        //simply asking how many rows are there in the table?
+            return 1
+    }
+    
+    internal func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        //Defines the contents of each cell
+        //(create cell & decide what you want in it
+        
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "Cell")
+        
+        cell.textLabel?.text = "Top row"
+        
+        return cell
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
