3464e2c712a4533d7fad8a339760a379bca77798
Doug Wells <dougwells@gmail.com>
2016-11-21 08:31:26 -0700

    iOS. TableViews. 50 labeled rows

--- tableView50/tableView50/ViewController.swift
+++ tableView50/tableView50/ViewController.swift
@@ -1,6 +1,6 @@
 //
 //  ViewController.swift
-//  tableViews
+//  tableView50
 //
 //  Created by Doug Wells on 11/21/16.
 //  Copyright © 2016 Doug Wells. All rights reserved.
@@ -10,26 +10,33 @@ import UIKit
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
-    var cellContent = ["Savannah","Sierra", "Linda","Doug"]
+    var countLimit:Int = 50
     
     internal func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        
         //simply asking how many rows are there in the table
-            return cellContent.count
+        
+        return countLimit
+
     }
     
+    
     internal func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
         //Defines the contents of each cell
         //(create cell & decide what you want in it
         
         let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "Cell")
         
-        cell.textLabel?.text = cellContent[indexPath.row]
-        
+        if countLimit > 0 {
+            cell.textLabel?.text = String(countLimit)
+            countLimit -= 1
+            
+        }
         return cell
         
     }
+    
+    
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
