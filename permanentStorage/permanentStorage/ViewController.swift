8a9d8a56690dd3d26c3172efc035ffe94a3a3f9b
Doug Wells <dougwells@gmail.com>
2016-11-22 08:30:01 -0700

    iOS. Permanent Storage. Simple save phone # app. Final Commit

--- permanentStorage/permanentStorage/ViewController.swift
+++ permanentStorage/permanentStorage/ViewController.swift
@@ -10,26 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var userPhone: UITextField!
+    
+    @IBOutlet weak var savedPhone: UILabel!
+    
+    @IBAction func savePhoneTapped(_ sender: Any) {
+        
+        UserDefaults.standard.set(userPhone.text, forKey: "phone")
+        userPhone.text = ""
+
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-    //Save name permantly
-//        UserDefaults.standard.set("Doug", forKey: "name")
-        let nameObject = UserDefaults.standard.object(forKey: "name")
-        if let name = nameObject as? String {
-            print(name)
+        let phoneObj = UserDefaults.standard.object(forKey: "phone")
+        if let phone = phoneObj as? String {
+            print(phone)
+            userPhone.text = phone
+            
         }
-        
-    //Save array permantly (NSArray is just a type of Array)
-    //See http://bit.ly/2gGrGM2
-        
-//        let scores = [95, 98, 75, 83]
-//        UserDefaults.standard.set(scores, forKey:"test1")
-        let test1ScoresObject = UserDefaults.standard.object(forKey: "test1")
-        if let test1Scores = test1ScoresObject as? NSArray {
-            print(test1Scores)
-        }
-        
+
     }
 
     override func didReceiveMemoryWarning() {
