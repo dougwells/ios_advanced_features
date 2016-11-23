fcbe2456576e3ccf995578c8802755437e838df3
Doug Wells <dougwells@gmail.com>
2016-11-23 07:42:28 -0700

    iOS. Multiple Views & textInput behaviors

---
+++ MultViewCntrls/MultViewCntrls/SecondViewController.swift
@@ -0,0 +1,36 @@
+//
+//  SecondViewController.swift
+//  MultViewCntrls
+//
+//  Created by Doug Wells on 11/23/16.
+//  Copyright © 2016 Doug Wells. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Second ViewController Loaded")
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ MultViewCntrls/MultViewCntrls/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  MultViewCntrls
+//
+//  Created by Doug Wells on 11/23/16.
+//  Copyright © 2016 Doug Wells. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITextFieldDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        print("FIRST ViewController Loaded")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+
+
+}
+
