621abe9f6ce0f42f49c7fd1f36f99a4ef4c5e64f
Doug Wells <dougwells@gmail.com>
2016-11-26 12:08:13 -0700

    iOS. Explore NSString methods.

---
+++ moreStrings.playground/Contents.swift
@@ -0,0 +1,35 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var salutation = "Hello"
+var name = "Doug"
+var greeting = salutation + " " + name
+print(greeting)
+
+for char in greeting.characters {
+    print(char)
+}
+
+//Convert to NSString type for more methods
+let newTypeGreeting = NSString(string: greeting)
+
+//NSString Methods
+
+    //Substring
+        newTypeGreeting.substring(to: 5)
+        newTypeGreeting.substring(from: 0)
+        NSString(string: newTypeGreeting.substring(from: 6)).substring(to:3)
+newTypeGreeting.substring(with: NSRange(location:6, length: 3))
+
+    //Contains character
+if newTypeGreeting.contains("Doug") { print("yep!") }
+
+    //Split longer string based on spaces
+
+newTypeGreeting.components(separatedBy: " ")//["Hello", "Doug"]
+
+    //Convert case
+    newTypeGreeting.uppercased
+    newTypeGreeting.lowercased
+
