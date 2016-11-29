261c78c737ca101e08b39f2fe312f5e4576df005
Doug Wells <dougwells@gmail.com>
2016-11-29 13:52:30 -0700

    iOS. Animations. Set up a timer (currently getting error).

--- Animations/Animations/ViewController.swift
+++ Animations/Animations/ViewController.swift
@@ -10,14 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var image: UIImageView!
+    @IBOutlet weak var startStopBtn: UIButton!
+    
     var count = 1
     
+    var timer = Timer.scheduledTimer(timeInterval: 1,
+        target: self,
+        selector: Selector(("animateFunction")),
+        userInfo: nil,
+        repeats: true)
+    
+ 
+    
+    
     @IBAction func nextTapped(_ sender: Any) {
-        if count > 5 {count = 0 }
-        let imageName = "frame_" + String(count) + "_delay-0.1s.gif"
-        image.image = UIImage(named: imageName)
-        count = count + 1
+        if startStopBtn.currentTitle == "Play" {
+            startStopBtn.setTitle("Pause",for: .normal)
         
+        } else {
+            timer.invalidate()
+            startStopBtn.setTitle("Play", for: .normal)
+            
+
+        }
+
+/*
+         if count > 5 {count = 0 }
+         let imageName = "frame_" + String(count) + "_delay-0.1s.gif"
+         image.image = UIImage(named: imageName)
+         count = count + 1
+*/
+
 /*
         for num in 1...5 {
            var imageName = "frame_" + String(num) + "_delay-0.1s.gif"
@@ -34,6 +57,12 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func animateFunction () {
+        print("Hello World!")
+        count = count + 1
+        if count > 5 {timer.invalidate()}
     }
 
 
