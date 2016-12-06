cab0a1f3240c157a4a57b7769a0b637bbb071018
Doug Wells <dougwells@gmail.com>
2016-12-06 13:52:26 -0700

    iOS. Mult Segues. Pass variables between views. Getting error. Next debug it

--- segueBetViews/SecondViewController.swift
+++ segueBetViews/SecondViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  SecondViewController.swift
 //  segueBetViews
 //
 //  Created by Doug Wells on 12/6/16.
@@ -8,15 +8,16 @@
 
 import UIKit
 
-let globalVariable = "Doug"
-
-class ViewController: UIViewController {
-    
+class SecondViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        print("VC1: ", globalVariable)
+        var username = "doug"
+        
+        print("VC 2: ", globalVariable, username)
+        
+
+        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
@@ -24,17 +25,15 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
-        
-        if segue.identifier == "toSecondViewController" {
-            let secondViewController = segue.destination as! SecondViewController
-            secondViewController.username = "linda"
-        }
     }
-
-
+    */
 
 }
-
