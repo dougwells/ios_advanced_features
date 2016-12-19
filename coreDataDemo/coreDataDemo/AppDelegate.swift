c3d57178de753bd958227ed192e5a54f02972c50
Doug Wells <dougwells@gmail.com>
2016-12-19 07:24:20 -0700

    ios. Core Cata. Init commit w/new filename

--- coreDataDemo/coreDataDemo/AppDelegate.swift
+++ coreDataDemo/coreDataDemo/AppDelegate.swift
@@ -1,6 +1,6 @@
 //
 //  AppDelegate.swift
-//  coreData
+//  coreDataDemo
 //
 //  Created by Doug Wells on 12/19/16.
 //  Copyright © 2016 Doug Wells. All rights reserved.
@@ -53,7 +53,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
          application to it. This property is optional since there are legitimate
          error conditions that could cause the creation of the store to fail.
         */
-        let container = NSPersistentContainer(name: "coreData")
+        let container = NSPersistentContainer(name: "coreDataDemo")
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error as NSError? {
                 // Replace this implementation with code to handle the error appropriately.
--- coreDataDemo/coreDataDemo/ViewController.swift
+++ coreDataDemo/coreDataDemo/ViewController.swift
@@ -1,20 +1,18 @@
 //
 //  ViewController.swift
-//  coreData
+//  coreDataDemo
 //
 //  Created by Doug Wells on 12/19/16.
 //  Copyright © 2016 Doug Wells. All rights reserved.
 //
 
 import UIKit
-import CoreData
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        let appDelegate = UIApplication.shared.delegate as! AppDelegate
     }
 
     override func didReceiveMemoryWarning() {
