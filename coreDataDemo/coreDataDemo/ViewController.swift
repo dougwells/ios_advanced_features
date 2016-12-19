f1114675dd835c06225a28c7f6a073a87bce9457
Doug Wells <dougwells@gmail.com>
2016-12-19 08:08:17 -0700

    ios. Core Data. Set up Users db. Have a bug to fix

--- coreDataDemo/coreDataDemo/ViewController.swift
+++ coreDataDemo/coreDataDemo/ViewController.swift
@@ -7,12 +7,60 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Gives us access to methods in AppDelegate.swift
+            let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        
+        /*
+         1) variable, "context" is similar to setting up "manager" that allowed us to work location or audio
+         2) persistentContainer is a container for persistent storage or CoreData
+         3) context gives us access to add & modify the data in the persistent container
+        */
+            let context = appDelegate.persistentContainer.viewContext
+        
+        //Create object that allows us to save a new user
+            let newUser = NSEntityDescription.insertNewObject(forEntityName: "Users", into: context)
+        
+        newUser.setValue("doug", forKey: "username")
+        newUser.setValue("myPass", forKey: "password")
+        newUser.setValue(35, forKey: "age")
+        
+        do {
+            
+            try context.save()
+            print("Saved")
+            
+        } catch {
+            print("Error in saving to Database. Do-Try-Catch")
+        }
+        
+        //Now retrieve the data (not must set .returnsObjectsAsFaults to false
+        
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Users")
+        request.returnsObjectsAsFaults = false
+        
+        do {
+            let results = try context.fetch(request)
+            
+            if results.count >0 {
+                for result in results as! [NSManagedObject] {
+                    if let username = result.value(forKey: "username") as? Sting {
+                        print(username)
+                    }
+                }
+            } else {
+                print("No results")
+            }
+        } catch {
+            print("Error retrieving data")
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
