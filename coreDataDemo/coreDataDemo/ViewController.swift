3a6100c7ab93ec830a2fc5289bf6facc0e74e722
Doug Wells <dougwells@gmail.com>
2016-12-22 09:20:29 -0700

    ios. Core Data. Add, Delete & Modify user data fields

--- coreDataDemo/coreDataDemo/ViewController.swift
+++ coreDataDemo/coreDataDemo/ViewController.swift
@@ -27,9 +27,11 @@ class ViewController: UIViewController {
         //Create object that allows us to save a new user
             let newUser = NSEntityDescription.insertNewObject(forEntityName: "Users", into: context)
         
-        newUser.setValue("sierra", forKey: "username")
+        /*
+        newUser.setValue("Ruth", forKey: "username")
         newUser.setValue("myPass", forKey: "password")
-        newUser.setValue(16, forKey: "age")
+        newUser.setValue(83, forKey: "age")
+        */
         
         do {
             
@@ -40,16 +42,45 @@ class ViewController: UIViewController {
             print("Error in saving to Database. Do-Try-Catch")
         }
         
-        //Now retrieve the data (not must set .returnsObjectsAsFaults to false
+        /* 
+         Retrieve data (note must set .returnsObjectsAsFaults to false)
+            - Swift defaults to returning just "faults" from DB query.
+            - Set to false so returns DB stored objects, not just faults
+        */
         
         let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Users")
+        
+        //request.predicate = NSPredicate(format: "%K > %D", "age", 59)
+        
+        //request.predicate = NSPredicate(format: "age < %@", "21")
+        
+        request.predicate = NSPredicate(format: "username = %@", "Doug")
+        
         request.returnsObjectsAsFaults = false
+        
         
         do {
             let results = try context.fetch(request)
             
             if (results.count > 0) {
+                
+                //loop thru array of result objects and print username
                 for result in results as! [NSManagedObject] {
+                    if let username = result.value(forKey: "username") as? String {
+                        print(username)
+                    }
+                    
+                    context.delete(result)
+                    
+                    do {
+                        try context.save()
+                        print("Delete succeeded")
+                        
+                    } catch {
+                        print("Delete Failed")
+                    }
+                    
+                    
                     if let username = result.value(forKey: "username") as? String {
                         print(username)
                     }
