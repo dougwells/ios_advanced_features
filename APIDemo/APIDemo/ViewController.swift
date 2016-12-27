8bc25abdbda7efb51c37a94ac6df3ba8b97be5ce
dougwells <dougwells@gmail.com>
2016-12-27 08:03:32 -0700

    ios. Using APIs. Init commit.

---
+++ APIDemo/APIDemo/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  APIDemo
+//
+//  Created by Doug Wells on 12/27/16.
+//  Copyright © 2016 Doug Wells. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        let url = URL(string: "http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=08e64df2d3f3bc0822de1f0fc22fcb2d")
+        
+        let task = URLSession.shared.dataTask(with: url!) { (data, response, error) in
+            <#code#>
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
