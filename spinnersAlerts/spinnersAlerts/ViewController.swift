0c2a95899a85a92d8ccc873c5332ff4b99611bba
dougwells <dougwells@gmail.com>
2017-01-04 07:30:30 -0700

    ios. Complex. Alerts & Spinners. Add alert.

--- spinnersAlerts/spinnersAlerts/ViewController.swift
+++ spinnersAlerts/spinnersAlerts/ViewController.swift
@@ -10,6 +10,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func createAlert(_ sender: Any) {
+        
+        // Structure of an alert line1: Title, line2: Message, line3: Button w/button title
+        
+        // Creates an alert (but does not display it)
+        let alertController = UIAlertController(title: "This is our alert", message: "Are you sure?", preferredStyle: UIAlertControllerStyle.alert)
+        
+        // Modify Alert's button & what happens when alert button is pressed
+        // Can have multiple buttons/actions at bottom of alert (ie "OK" & "Cancel")
+        
+        alertController.addAction(UIAlertAction(title: "OK", style: .default , handler: { (action) in
+            
+            print("OK Pressed")
+            self.dismiss(animated: true, completion: nil)
+                }))
+        
+        alertController.addAction(UIAlertAction(title: "Cancel", style: .default , handler: { (action) in
+            
+            print("Cancel Pressed")
+            self.dismiss(animated: true, completion: nil)
+        }))
+        
+        //Present Alert
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
+    @IBAction func pauseApp(_ sender: Any) {
+    }
+    
+    @IBAction func resumeApp(_ sender: Any) {
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
