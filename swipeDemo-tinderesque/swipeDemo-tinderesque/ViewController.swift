cb34e5ade561dc87d1b6b63c563b5790434b3d43
dougwells <dougwells@gmail.com>
2017-01-15 07:26:33 -0700

    ios. Complex. Tinder swipe. Add view element programatically (vs via storyboard).

--- swipeDemo-tinderesque/swipeDemo-tinderesque/ViewController.swift
+++ swipeDemo-tinderesque/swipeDemo-tinderesque/ViewController.swift
@@ -13,6 +13,14 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let label = UILabel(frame: CGRect(x: self.view.bounds.width/2 - 100, y: self.view.bounds.height/2 - 50, width: 200, height: 100))
+        
+        label.text = "Drag me"
+        label.textAlignment = NSTextAlignment.center
+        
+        view.addSubview(label)
+        
     }
 
     override func didReceiveMemoryWarning() {
