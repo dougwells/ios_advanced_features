67476f4bac663fb55d897ab96c86ceb41b8346aa
dougwells <dougwells@gmail.com>
2017-01-15 07:45:48 -0700

    ios. Complex. Tinder swipe. Label recgonizes drag gesture

--- swipeDemo-tinderesque/swipeDemo-tinderesque/ViewController.swift
+++ swipeDemo-tinderesque/swipeDemo-tinderesque/ViewController.swift
@@ -14,12 +14,26 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        //Add element to the screen programatically (w/o storyboard)
         let label = UILabel(frame: CGRect(x: self.view.bounds.width/2 - 100, y: self.view.bounds.height/2 - 50, width: 200, height: 100))
         
         label.text = "Drag me"
         label.textAlignment = NSTextAlignment.center
         
         view.addSubview(label)
+        
+        //Recognizer user gesture "pan"/swipe & run fn "wasDragged"
+        let gesture = UIPanGestureRecognizer(target: self, action: #selector(self.wasDragged(gestureRecognizer:)))
+        
+        //Make label interactive (by default a label is not)
+        label.isUserInteractionEnabled = true
+        label.addGestureRecognizer(gesture)
+        
+    }
+    
+    func wasDragged(gestureRecognizer: UIPanGestureRecognizer) {
+        
+        print("Was dragged")
         
     }
 
